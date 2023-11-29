5cc77b9900c9adedf9f24ae2ef61a0ecc6c40e96
CristianBMJ <cristianj3006@gmail.com>
2023-11-29 10:25:41 -0300

    Add test_transform.py

---
+++ Test/test_transform.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from transform import transform_data_with_pandas
+
+def test_transformed_data_is_dataframe():
+    df = transform_data_with_pandas()
+    assert isinstance(df, pd.DataFrame)
+
+def test_transformed_data_no_duplicates():
+    df = transform_data_with_pandas()
+    if df is not None:
+        assert not df.duplicated(subset='id').any()
+
+def test_transformed_data_no_missing_values():
+    df = transform_data_with_pandas()
+    if df is not None:
+        assert not df['title'].isnull().any()
+
+def test_transformed_data_title_is_uppercase():
+    df = transform_data_with_pandas()
+    if df is not None:
+        assert all(df['title'].str.isupper())
